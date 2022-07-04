1346656103d0dc1e1e46f30b669412a1e16bdb55
onurcan <onurcanaylin@gmail.com>
2022-07-04 17:04:18 +0300

    commit on 'master'

---
+++ src/main/java/com/example/demo/model/Piece.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+public class Piece {
+    public String color;
+    public Piece(String color){
+        this.color=color;
+    }
+}
---
+++ src/main/java/com/example/demo/model/Quenn.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+public class Quenn extends Piece {
+
+    public Quenn(String color) {
+        super(color);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
